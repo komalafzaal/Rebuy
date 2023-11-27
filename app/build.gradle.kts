d76a98b447f3f0510a07246eecbf3e1d9b6ea048
komalafzaal <komalafzaal24@gmail.com>
2023-11-27 13:30:26 +0500

    splash screen done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
---
+++ app/src/main/java/com/example/rebuy/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.rebuy
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Timer
+import java.util.TimerTask
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val task: TimerTask = object : TimerTask() {
+            override fun run() {
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        val t = Timer()
+        t.schedule(task, 3000)
+
+    }
+}
