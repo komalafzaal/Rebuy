f9281a1d3d6bc51df31b469600746d085e7099ed
komalafzaal <komalafzaal24@gmail.com>
2023-11-27 16:41:29 +0500

    login screen done

--- app/src/main/java/com/example/rebuy/Login.kt
+++ app/src/main/java/com/example/rebuy/Login.kt
@@ -1,11 +1,20 @@
 package com.example.rebuy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val signup : TextView = findViewById(R.id.signup_text)
+
+        signup.setOnClickListener{
+            val intent = Intent(this@Login, SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
